90236dfce7c1e712e35bf50e2da566bc25a8ce28
Leonid Holub <rsleonid@gmail.com>
2023-06-12 21:52:29 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/meddiagnostic/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
