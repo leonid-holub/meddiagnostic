db7263698e4d25c3bbd3bddb871d148bfc8c0514
Leonid Holub <rsleonid@gmail.com>
2023-06-13 03:25:40 +0300

    Update

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
     return (
         <div className={style.logo}>
             <svg className={style.logo__img}  width="45" height="40">
-                <use href="/images/icons.svg#meddiagnostic">
+                <use href="images/icons.svg#meddiagnostic">
                 </use>
             </svg>
             <p className={style.logo__name}>ТД &quot;МЕД <br/>ДІАГНОСТИК&quot;</p>
